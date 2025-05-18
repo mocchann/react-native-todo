69a0f1bcae4b18eecbddb4df22bb3a5bbcfe54b4
もっちゃん <s7056i3056@gmail.com>
2025-05-18 13:11:28 +0900

    feat: index header

--- app/(home)/todo/index.tsx
+++ app/(home)/todo/index.tsx
@@ -1,10 +1,49 @@
-import { Text, View } from "react-native";
+import { Link, Stack } from "expo-router";
+import { Image, StyleSheet, View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 50,
+    height: 50,
+  },
+  title: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+});
+
+function LogoTitle() {
+  return (
+    <View style={styles.title}>
+      <Image
+        style={styles.image}
+        source={{ uri: "https://reactnative.dev/img/tiny_logo.png" }}
+      />
+      <Link href={"/todo"}>Todo Index</Link>
+    </View>
+  );
+}
 
 const TodoIndex = () => {
   return (
-    <View>
-      <Text>todo index</Text>
-    </View>
+    <Stack.Screen
+      options={{
+        title: "My home",
+        headerStyle: { backgroundColor: "#f4511e" },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+
+        headerTitle: () => <LogoTitle />,
+      }}
+    />
   );
 };
 
