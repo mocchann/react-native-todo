856944dcc5a23c9ca48314c06d49035d449eeab2
もっちゃん <s7056i3056@gmail.com>
2025-05-20 22:01:32 +0900

    feat: apiからtodo取得・表示処理を実装

--- app/(home)/todo/index.tsx
+++ app/(home)/todo/index.tsx
@@ -1,5 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
 import { Stack } from "expo-router";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -16,7 +23,40 @@ const styles = StyleSheet.create({
     alignItems: "center",
     gap: 10,
   },
+  todoItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+    backgroundColor: "#fff",
+    marginVertical: 4,
+    borderRadius: 8,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 3,
+  },
+  todoText: {
+    fontSize: 16,
+  },
+  completedText: {
+    textDecorationLine: "line-through",
+    color: "#888",
+  },
 });
+
+const fetchTodos = async () => {
+  const res = await fetch("http://example.com/api/");
+
+  if (!res.ok) {
+    throw new Error("api response was not ok");
+  }
+
+  return res.json();
+};
 
 function LogoTitle() {
   return (
@@ -31,19 +71,50 @@ function LogoTitle() {
 }
 
 const TodoIndex = () => {
-  return (
-    <Stack.Screen
-      options={{
-        title: "My home",
-        headerStyle: { backgroundColor: "#f4511e" },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
+  const {
+    data: todos,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["todos"],
+    queryFn: fetchTodos,
+  });
 
-        headerTitle: () => <LogoTitle />,
-      }}
-    />
+  const renderItem = ({ item }: { item: Todo }) => (
+    <View style={styles.todoItem}>
+      <Text style={[styles.todoText, item.completed && styles.completedText]}>
+        {item.title}
+      </Text>
+    </View>
+  );
+
+  return (
+    <>
+      <Stack.Screen
+        options={{
+          title: "My home",
+          headerStyle: { backgroundColor: "#f4511e" },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+
+          headerTitle: () => <LogoTitle />,
+        }}
+      />
+      {isLoading ? (
+        <Text>Loading...</Text>
+      ) : error ? (
+        <Text>Error: {error?.message}</Text>
+      ) : (
+        <FlatList
+          data={todos}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id.toString()}
+          style={styles.container}
+        />
+      )}
+    </>
   );
 };
 
