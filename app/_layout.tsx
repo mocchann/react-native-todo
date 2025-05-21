c1010138839b67394f20bf43f455440714fd1d26
もっちゃん <s7056i3056@gmail.com>
2025-05-21 23:00:00 +0900

    feat: react-query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack>
+        <Stack.Screen name="(home)" />
+      </Stack>
+    </QueryClientProvider>
+  );
 }
